64148063c77a4c116c9d73c181587c3c47abaa8e
klbrosemer-glitch <klbrosemer@gmail.com>
2026-09-18 10:12:27 -0500

    Added Main battle loop

--- game.py
+++ game.py
@@ -1,7 +1,25 @@
 from goblin import Goblin
+from hero import Hero
 
 
-ARENA_NAME = "The Iron Lung"
+
+ARENA_NAME = "The Wolf Pack"
+
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+    if hero.is_alive():
+        print(f"{hero.name} has defeated {enemy.name}!")
+    else:
+        print(f"{enemy.name} has defeated {hero.name}!")
+
+
 
 
 def main():
@@ -9,12 +27,18 @@ def main():
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
+    
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("67 Skibiti Toilet")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+
+    hero = Hero("Glutius Maximus")
+    print(f"{hero.name} enters the arena with {hero.health} health.")
+    print("The battle begins!")
+    battle(hero, goblin)
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,25 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random
 
-    pass
+
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 20
+        self.defense = 5
+
+    def attack(self):
+        passedCritCheck=random.random(1,6) == 6 
+        if passedCritCheck:
+            return random.randint(1, self.attack_power) * 1.25
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        # Subtract damage, but do not allow health to fall below 0.
+        self.health = self.health - damage
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
+        return self.health > 0
+
